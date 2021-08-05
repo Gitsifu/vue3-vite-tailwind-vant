e6ef2585251cde53e0084310c853c8f07709c395
sifu <2587107273@qq.com>
2021-08-05 10:42:38 +0800

    add vuex@4

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,11 +1,14 @@
 <template>
     <div class="tw-flex tw-flex-col tw-items-center">
         <div class="tw-m-[100px] tw-text-3xl">{{ msg }}</div>
-        <van-button type="primary" @click="count++">count is: {{ count }}</van-button>
+        <van-button type="primary" @click="count++">count is: {{ count }}</van-button><br/>
+        <van-button type="primary" @click="increment()">storeCount is: {{ storeCount }}</van-button>
     </div>
 </template>
 
 <script>
+import { useStore }  from"vuex"
+import {computed,ref} from "vue";
 export default {
     name: 'HelloWorld',
     props: {
@@ -13,7 +16,23 @@ export default {
     },
     data() {
         return {
-            count: 0
+        }
+    },
+    setup(props,ctx) {
+        const store = useStore()
+
+        const count = ref(0)
+
+        const storeCount = computed(() => store.state.count)
+
+        const increment = () => {
+            store.commit('increment')
+        }
+
+        return {
+            count,
+            storeCount,
+            increment
         }
     }
 }
--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router/index.js'
+import store from './store/index.js'
 import Vant from 'vant';
 import 'vant/lib/index.css';
 import './index.css'
 
 const app = createApp(App)
 app.use(router)
+app.use(store)
 app.use(Vant)
 app.mount('#app')
