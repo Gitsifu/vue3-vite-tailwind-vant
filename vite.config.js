982343c6058df2dadaf08ab3863c2bfca0e37963
sifu <2587107273@qq.com>
2021-08-13 16:21:11 +0800

    添加rollup-plugin-visualizer和@vitejs/plugin-legacy

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,21 @@
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
 import {defineConfig} from "vite";
+import legacy from '@vitejs/plugin-legacy'
+import { visualizer } from 'rollup-plugin-visualizer';
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    legacy({
+      targets: ['defaults', 'not IE 11']
+    }),
+    process.env.vis && visualizer({
+      open: true,
+      gzipSize: true,
+      brotliSize: true
+    })
+  ],
   resolve: {
     alias: [
       { find: '@', replacement: resolve(__dirname, 'src') },
